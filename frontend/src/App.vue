6870e92c06952240698ba74df3b26dd847269c05
Sergio Sobrado <sergiomanuel1702@gmail.com>
2024-03-02 21:10:36 -0800

    Added HomeView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
+div {
+  height: 100vh;
+}
 nav {
   padding: 30px;
 }
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="sideBar">
+    <div class="sideBarDesign">
+        <router-link 
+        to="/about" class="links">
+            <div class="linkPageIcons">
+                <img src="@/assets/user.png" width="70" alt="inicio">
+                <h3 class="linkText">Inicio</h3>
+            </div>
+        </router-link>     
+        <router-link 
+        to="/" class="links">
+            <div class="linkPageIcons">
+                <img src="@/assets/settings.png" width="70" alt="inicio">
+                <h3 class="linkText">Diagnosticar</h3>
+            </div>
+        </router-link>   
+        <router-link 
+        to="/" class="links">
+            <div class="linkPageIcons">
+                <img src="@/assets/visualizer.png" width="70" alt="inicio">
+                <h3 class="linkText">Visualizar</h3>
+            </div>
+        </router-link>   
+        <router-link 
+        to="/" class="links">
+            <div class="linkPageIcons">
+                <img src="@/assets/manual.png" width="70" alt="inicio">
+                <h3 class="linkText">Manual</h3>
+            </div>
+        </router-link>   
+    </div>
+  </div>
+</template>
+
+<script lang="ts"> import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "SideBar",
+    data() {
+        return {
+        }
+    } 
+});
+</script>
+
+<style scoped>
+.sideBar {
+    min-width: 300px;
+    background : linear-gradient(0deg, #1F292B, #1F292B),
+    linear-gradient(180deg, rgba(122, 39, 206, 0.2) 0%, rgba(116, 104, 128, 0.2) 100%);
+    background-color: rgba(31, 41, 43, 1);
+    padding: 10px;
+}
+.sideBarDesign {    
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 25px;
+}
+.linkPageIcons {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+    
+}
+.links {
+    text-decoration: none;
+    color: white;
+}
+@media screen and (max-width: 560px)  {
+  .sideBar {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .sideBarDesign {
+    justify-content: center;
+    height: 100vh;
+  }
+}
+</style>    
