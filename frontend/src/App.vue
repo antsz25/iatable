2b5754710b7235296d1a74f2af3954b71ff2e77c
Sergio Sobrado <sergiomanuel1702@gmail.com>
2024-03-20 19:08:51 -0700

    Image on top bar change dynamically

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
-    <div class="top-bar">
-      <img src="/" alt="inico">
+    <div class="top-bar" v-if="$route.path!='/'">
+      <img :src="require(`@/assets/${getActualURL}.png`)" alt="Inicio">
+      <img src="@/assets/exit.png" alt="">
     </div>
     <router-view/>
   </div>
 </template>
+
+
+<script lang="ts">
+export default {
+  data() {
+    return{
+      actualRoute: ""
+    }
+  },
+  computed: {
+    getActualURL(): string {
+      if(this.$route.path=="/diagnostico"){
+        return "settings"
+      }
+      return "home"
+    },
+  }
+
+}
+</script>
 
 <style scoped>
 #app {
@@ -29,5 +50,10 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.top-bar {
+  background: linear-gradient(180deg, rgba(43, 60, 215, 0.2) 0%, rgba(116, 104, 128, 0.2) 100%), #000000;
+  height: 63px;
 }
 </style>
